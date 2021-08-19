8bb805931260650df28940d73a1af8343fed8974
Rupinderj <65835380+Rupinderj@users.noreply.github.com>
2021-08-18 23:07:10 -0400

    Update BlockSummary.cs

--- CP380-B2-BlockWebAPI/Models/BlockSummary.cs
+++ CP380-B2-BlockWebAPI/Models/BlockSummary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CP380_B1_BlockList.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace CP380_B2_BlockWebAPI.Models
     public class BlockSummary
     {
         // TODO
+           
+        public DateTime Timedate { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+    
+
+    public BlockSummary(DateTime TimeStamp, string PreviousHash, string Hash)
+    {
+        this.Timedate = TimeStamp;
+        this.PreviousHash = PreviousHash;
+        this.Hash = Hash;
     }
-}
+    public  BlockSummary()
+    {
+
+    }
+    }
+     
+ }
