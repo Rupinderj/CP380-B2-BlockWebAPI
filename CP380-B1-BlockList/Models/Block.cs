ca2c78ef0d42272b0b25889d4d17aad49133a4f7
Rupinderj <65835380+Rupinderj@users.noreply.github.com>
2021-08-19 12:10:40 -0400

    Update Block.cs

--- CP380-B1-BlockList/Models/Block.cs
+++ CP380-B1-BlockList/Models/Block.cs
@@ -36,7 +36,7 @@ namespace CP380_B1_BlockList.Models
             //
             // TODO
             //
-            var inputString = $""; // TODO
+            var inputString = $"{TimeStamp.Date:yyyy-MM-dd hh:mm:ss tt}-{PreviousHash}-{Nonce}-{json}"; // TODO
 
             var inputBytes = Encoding.ASCII.GetBytes(inputString);
             var outputBytes = sha256.ComputeHash(inputBytes);
