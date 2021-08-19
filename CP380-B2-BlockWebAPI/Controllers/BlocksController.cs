eb79d14ca9c394dda7913f2d017a07b037a772ad
Rupinderj <65835380+Rupinderj@users.noreply.github.com>
2021-08-18 23:07:21 -0400

    Update BlocksController.cs

--- CP380-B2-BlockWebAPI/Controllers/BlocksController.cs
+++ CP380-B2-BlockWebAPI/Controllers/BlocksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,5 +15,65 @@ namespace CP380_B2_BlockWebAPI.Controllers
     public class BlocksController : ControllerBase
     {
         // TODO
+         public BlockList lista;
+
+        public BlocksController(BlockList blockList)
+        {
+            lista = blockList;
+        }
+
+
+        [HttpGet("/blocks")]
+        public IActionResult Get()
+        {
+            return Ok(lista.Chain.Select(a => new BlockSummary()
+            {
+                Hash = a.Hash,
+                PreviousHash = a.PreviousHash,
+                TimeStamp = a.TimeStamp,
+                
+            })); 
+        }
+
+        
+        [HttpGet("/blocks/{hash?}")]
+        public IActionResult GetBlock(string hash)
+        {
+            var block = lista.Chain
+                .Where(block => block.Hash.Equals(hash));
+
+            if (block != null && block.Count() > 0)
+            {
+                return Ok(block
+                    .Select(block1 => new BlockSummary()
+                    {
+                        Hash = block1.Hash,
+                        PreviousHash = block1.PreviousHash,
+                        TimeStamp = block1.TimeStamp,
+                        
+                    }
+                    )
+                    .First());
+            }
+
+            return null;
+        }
+
+        [HttpGet("/ blocks /{hash ?}/payloads")]
+        public IActionResult GetBlockPayload(string Hash)
+        {
+            var block = lista.Chain
+                        .Where(block => block.Hash.Equals(Hash));
+
+            if (block != null && block.Count() > 0)
+            {
+                return Ok(block
+                    .Select(block => block.Data
+                    )
+                    .First());
+            }
+
+            return null;
+        }
     }
 }
