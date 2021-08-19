7aad0d1f807b6c77233af9fb481d82d024e2bf55
Rupinderj <65835380+Rupinderj@users.noreply.github.com>
2021-08-19 01:06:39 -0400

    Create Payload.cs

---
+++ CP380-B1-BlockList/Models/Payload.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CP380_B1_BlockList.Models
+{
+    public enum TransactionTypes
+    {
+        BUY, SELL, GRANT
+    }
+
+   public class Payload
+    {
+        public string user { get; set; }
+        public TransactionTypes transactionType { get; set; }
+        public int amount { get; set; }
+        public string item { get; set; }
+
+        public Payload(string user, TransactionTypes transaction, int amount, string item)
+        {
+            this.user = user;
+            this.transactionType = transaction;
+            this.amount = amount;
+            this.item = item;
+        }
+    }
+}
